5472a8122a383eedb6e01410f709bccf0735bcc4
adwisatya <a.dwisaty4@yahoo.com>
2015-04-09 23:21:11 +0700

    a

--- bin/upload.php
+++ bin/upload.php
@@ -6,12 +6,14 @@
 	$file = str_replace(".php",".php.txt",$_FILES['userfile']['name']);
 	$target = "../img/laporan/";
 	$path = $target.md5($_FILES['userfile']['name']).".".pathinfo($file, PATHINFO_EXTENSION);
+			$fileHandler = new File();
 	if(move_uploaded_file($_FILES['userfile']['tmp_name'],$path)){
-		$fileHandler = new File();
 		$fileHandler->addToDB($tanggal,$deskripsi,md5($_FILES['userfile']['name']).".".pathinfo($file, PATHINFO_EXTENSION));
 		//echo "<script>alert('File Uploaded Successfully');</script>";
 		header("location: ../laporan-harian.php");
 	}else{
+		$fileHandler->addToDB($tanggal,$deskripsi," ");
+
 		header("location: ../laporan-harian.php");
 
 	}
--- bin/upload2.php
+++ bin/upload2.php
@@ -5,12 +5,15 @@
 	$file = str_replace(".php",".php.txt",$_FILES['userfile']['name']);
 	$target = "../img/laporan/";
 	$path = $target.md5($_FILES['userfile']['name']).".".pathinfo($file, PATHINFO_EXTENSION);
-	if(move_uploaded_file($_FILES['userfile']['tmp_name'],$path)){
 		$fileHandler = new File();
+
+	if(move_uploaded_file($_FILES['userfile']['tmp_name'],$path)){
 		$fileHandler->addToDB2($minggu, md5($_FILES['userfile']['name']).".".pathinfo($file, PATHINFO_EXTENSION));
 		//echo "<script>alert('File Uploaded Successfully');</script>";
 		header("location: ../laporan-mingguan.php");
 	}else{
+		$fileHandler->addToDB2($minggu, "");
+
 		header("location: ../laporan-mingguan.php");
 
 	}
